af05c4e9ac66c66a28ee9ecc3ed1cdc1963a3428
GreatenAnonymous <2274880117@qq.com>
2025-05-17 00:31:08 -0400

    add example

---
+++ 2d/rth/demo/run.sh
@@ -0,0 +1,3 @@
+#python ./visualize.py ./small.yaml small_demo.yaml --speed 1000
+#python ./visualize.py ./block_demo_small.yaml output_block_small.yaml --speed 1000
+python ./visualize2.py ./demo_small_obs.yaml output_small_obs.yaml --speed 1000
---
+++ 2d/rth/demo/run2.sh
@@ -0,0 +1,3 @@
+#python ./visualize.py ./small.yaml small_demo.yaml --speed 1000
+python ./visualize2.py ./demo_block_small.yaml test.yaml --speed 1000
+    
